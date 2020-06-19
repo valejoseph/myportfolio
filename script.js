35b222d3b5fba0232ef75ee4e46725e6ea3f69f4
valejoseph <valejoseph@hotmail.com>
2020-06-19 16:06:27 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 window.onload = function(){
     alert("Thank you for visiting my portfolio!");
-    let name = this.prompt ("What is your name?");
-    let domname = this.document.getElementById('name');
-    domname.innerHTML= 'Welcome! ' + name;
+    let name123 = this.prompt ("What is your name?");
+    let domname = this.document.getElementById('name123');
+    domname.innerHTML= 'Welcome! ' + name123;
+};   
 
-    var first_num = prompt("Enter the first number", "");
-    var second_num = prompt("Enter the second number", "");
-    var answer = first_num + second_num;
-    let message = this.document.getElementById('test')
-    message.innerHTML=("The sums of the two number is = " + answer);
-
-    let i =answer
-    if (i>10){
-        this.alert("This is a big number");
-    }
-        else if (i<=10)
-        {
-            this.alert("this is a small number");
-        }
-        else{
-        this.alert('Thank you');
-        }
-
-
-
-    }
+function myvalidator() {
+    var x = document.forms["myForm"]["firstName"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+  }
+  {
+    var x = document.forms["myForm"]["lastName"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+  }
+  
+  }
+  
+  }
+    
+ 
+  
    
-
-
+  
+  
+  
+      
+  
+  
+  
+  
 
 
 
